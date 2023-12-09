01722209f149be6f8ad617e9410803e37a222df1
AngeloRaffaele <ralf9997@gmail.com>
2023-12-09 13:01:06 +0100

    es

---
+++ Dom/Storage/session.js
@@ -0,0 +1,22 @@
+const btn = document.querySelector(".btn");
+
+function setData(firstname){
+    const name = document.querySelector(".title-name");
+
+    name.innerText = firstname;
+    
+    sessionStorage.setItem("title-name", firstname);
+}
+
+btn.addEventListener("click", () => {
+    const input = document.querySelector(".firstname");
+    const value = input.value;
+
+    setData(value);
+});
+
+const localSave = localStorage.getItem("title-name") ;
+
+if (localSave) {
+    setData(localSave);
+}
