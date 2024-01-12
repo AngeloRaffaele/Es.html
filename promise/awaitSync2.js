183fbb117d03b9999c79148a4e6be52e29d7ec08
AngeloRaffaele <ralf9997@gmail.com>
2024-01-12 19:30:33 +0100

    es

--- promise/awaitSync2.js
+++ promise/awaitSync2.js
@@ -5,3 +5,21 @@ function caricaModulo(modulo) {
       }, 2000);
     });
   }
+
+  
+async function lanciaVeicoloSpaziale() {
+  try {
+    const A = await caricaModulo("A");
+    console.log(A);
+    const B = await caricaModulo("B");
+    console.log(B);
+    const C = await caricaModulo("C");
+    console.log(C);
+    const messaggioFinale = "Veicolo spaziale pronto per il lancio!";
+    console.log(messaggioFinale);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+lanciaVeicoloSpaziale();
